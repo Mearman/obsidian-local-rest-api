88bf4000eae36b21fc61feb48589b5ecb317d60b
Joseph Mearman <joseph@mearman.co.uk>
2022-05-18 13:00:00 +0100

    update build output dir

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -50,6 +50,6 @@ esbuild
     logLevel: "info",
     sourcemap: prod ? false : "inline",
     treeShaking: true,
-    outfile: "main.js",
+		outfile: "./dist/main.js",
   })
   .catch(() => process.exit(1));
